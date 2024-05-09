c1b68a47559a8d19c48448fe9675d3241e43d9f5
imzino24z <imadkoultouma023@gmail.com>
2024-05-09 17:21:08 +0100

    attempted to do practical 7

--- Testing3/tstOrder.cs
+++ Testing3/tstOrder.cs
@@ -15,5 +15,18 @@ namespace Testing3
             //test to see that it exists
             Assert.IsNotNull(AnOrder);
         }
+
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we weant to create 
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property 
+            Boolean TestData = true;
+            //assign the data to the property 
+            AnOrder.Active = TestData;
+            //test to see that the two values are the same 
+            Assert.AreEqual(AnOrder.Active, TestData);
+        }
     }
 }
