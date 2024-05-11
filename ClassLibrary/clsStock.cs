8c912a4fe65234b10ba6c5c7787f847a37a47fdf
KeerthanaC9 <keera200130@gmail.com>
2024-05-11 13:00:29 +0100

    Created and tested properties for my table

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,16 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public bool Available { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public int ShoeId { get; set; }
+        public string ShoeName { get; set; }
+        public string Supplier { get; set; }
+        public int ShoeSize { get; set; }
+        public string ShoeColor { get; set; }
+        public decimal ShoePrice { get; set; }
     }
 }
--- Testing4/tstStock.cs
+++ Testing4/tstStock.cs
@@ -15,5 +15,111 @@ namespace Testing4
             //test to see that it exists
             Assert.IsNotNull(AStock);        
         }
-    }
+
+        [TestMethod]
+
+        public void AvailablePropertyOK()
+        {
+            clsStock AStock = new clsStock();
+
+            Boolean TestData = true;
+
+            AStock.Available = TestData;
+
+            Assert.AreEqual(AStock.Available, TestData);
+        }
+
+        [TestMethod]
+
+        public void DateUpdatedPropertyOK()
+        {
+
+            clsStock AStock = new clsStock();
+
+            DateTime TestData = DateTime.Now.Date;
+
+            AStock.DateUpdated = TestData;
+
+            Assert.AreEqual(AStock.DateUpdated, TestData);
+        }
+
+        [TestMethod]
+
+        public void ShoeIdPropertyOK()
+        {
+            clsStock AStock = new clsStock();
+
+            Int32 TestData = 1;
+
+            AStock.ShoeId = TestData;
+
+            Assert.AreEqual(AStock.ShoeId, TestData);
+        }
+
+        [TestMethod]
+
+        public void ShoeNamePropertyOK()
+        {
+            clsStock AStock = new clsStock();
+
+            string TestData = "Nike Air Force 1";
+
+            AStock.ShoeName = TestData;
+
+            Assert.AreEqual(AStock.ShoeName, TestData);
+        }
+
+        [TestMethod]
+
+        public void SupplierPropertyOK()
+        {
+            clsStock AStock = new clsStock();
+
+            string TestData = "Nike";
+
+            AStock.Supplier = TestData;
+
+            Assert.AreEqual(AStock.Supplier, TestData);
+        }
+
+        [TestMethod]
+
+        public void ShoeSizePropertyOK()
+        {
+            clsStock AStock = new clsStock();
+
+            Int32 TestData = 6;
+
+            AStock.ShoeSize = TestData;
+
+            Assert.AreEqual(AStock.ShoeSize, TestData);
+        }
+
+        [TestMethod]
+
+        public void ShoeColorPropertyOK()
+        {
+            clsStock AStock = new clsStock();
+
+            string TestData = "blue";
+
+            AStock.ShoeColor = TestData;
+
+            Assert.AreEqual(AStock.ShoeColor, TestData);
+        }
+
+        [TestMethod]
+
+        public void ShoePricePropertyOK()
+        {
+            clsStock AStock = new clsStock();
+
+            decimal TestData = 70.00m;
+
+            AStock.ShoePrice = TestData;
+
+            Assert.AreEqual(AStock.ShoePrice, TestData);
+        }
+
+        }
 }
