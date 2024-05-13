7009625f899668332401953e034a42408d8b7be6
imzino24z <imadkoultouma023@gmail.com>
2024-05-13 22:14:58 +0100

    This is the properties for the table i created as asked to do so in poractical 7

--- Testing3/tstOrder.cs
+++ Testing3/tstOrder.cs
@@ -1,4 +1,5 @@
 ﻿using ClassLibrary;
+using FsCheck;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
@@ -17,7 +18,7 @@ namespace Testing3
         }
 
         [TestMethod]
-        public void InstanceOK()
+        public void ActiveOrderOK()
         {
             //create an instance of the class we weant to create 
             clsOrder AnOrder = new clsOrder();
@@ -28,5 +29,84 @@ namespace Testing3
             //test to see that the two values are the same 
             Assert.AreEqual(AnOrder.Active, TestData);
         }
+
+        [TestMethod]
+        public void DateAddedOrderOK()
+        {
+            //create an instance of the class we want to create 
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property 
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property 
+            AnOrder.DateAdded = TestData;
+            //test to see that the two values are the same 
+            Assert.AreEqual(AnOrder.DateAdded, TestData);
+        }
+
+        [TestMethod]
+        public void OrderIdOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AnOrder.OrderId = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.OrderId, TestData);
+        }
+
+        [TestMethod]
+        public void ProductIdOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AnOrder.ProductId = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.ProductId, TestData);
+        }
+
+        [TestMethod]
+        public void CustomerIdOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AnOrder.CustomerId = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.CustomerId, TestData);
+        }
+
+        [TestMethod]
+        public void OrderDeliveredOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            bool TestData = true;
+            //assign the data to the property
+            AnOrder.OrderDelivered = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.OrderDelivered, TestData);
+        }
+
+        [TestMethod]
+        public void TotalPriceOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            float TestData = 100.0f;
+            //assign the data to the property
+            AnOrder.TotalPrice = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.TotalPrice, TestData);
+        }
+
     }
 }
