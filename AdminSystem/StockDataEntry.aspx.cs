78e1b48c45dd8b3ec3a7de5bd9dbcd04ca79764c
KeerthanaC9 <keera200130@gmail.com>
2024-05-14 19:24:33 +0100

    added presentation layer

--- AdminSystem/StockDataEntry.aspx.cs
+++ AdminSystem/StockDataEntry.aspx.cs
@@ -21,20 +21,7 @@ public partial class _1_DataEntry : System.Web.UI.Page
         clsStock AStock = new clsStock();
         //capture the shoe id
         AStock.ShoeId = Convert.ToInt32(txtShoeId.Text);
-        //capture the shoe name
-        AStock.ShoeName = txtShoeName.Text;
-        //capture the supplier
-        AStock.Supplier = txtSupplier.Text;
-        //capture the shoe size 
-        AStock.ShoeSize = Convert.ToInt32(txtShoeSize.Text);
-        //capture the shoe color
-        AStock.ShoeColor = txtShoeColor.Text;
-        //captrue the shoe price
-        AStock.ShoePrice = Convert.ToDecimal(txtShoePrice.Text);
-        //capture the date updated
-        AStock.DateUpdated = Convert.ToDateTime(DateTime.Now);
-        //capture available check box
-        AStock.Available = chkAvailable.Checked;
+        
         //store the address in the session object
         Session["AStock"] = AStock;
         //navigate to the view page
--- AdminSystem/StockViewer.aspx.cs
+++ AdminSystem/StockViewer.aspx.cs
@@ -16,20 +16,8 @@ public partial class _1Viewer : System.Web.UI.Page
         AStock = (clsStock)Session["AStock"];     
         //display the shoe id for this entry
         Response.Write(AStock.ShoeId);
-        //display the shoe name for this entry
-        Response.Write(AStock.ShoeName);
-        //display the supplier for this entry
-        Response.Write(AStock.Supplier);
-        //display the shoe size for this entry
-        Response.Write(AStock.ShoeSize);
-        //display the shoe color for this entry
-        Response.Write(AStock.ShoeColor);
-        //display the shoe price for this entry
-        Response.Write(AStock.ShoePrice);
-        //display the date updated for this entry
-        Response.Write(AStock.DateUpdated);
-        //display the available check box
-        Response.Write(AStock.Available);
+        
+      
 
 
     }
