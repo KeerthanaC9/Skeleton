2fb017b713c886a839d0656db72fa401fc4176b4
its-ym <161828938+its-ym@users.noreply.github.com>
2024-05-16 10:58:43 +0100

    update

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using ClassLibrary;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
@@ -15,5 +17,17 @@ public partial class _1_DataEntry : System.Web.UI.Page
     protected void Unnamed2_TextChanged(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsCustomer
+        clsCustomer AnCustomer = new clsCustomer();
+        //capture the name
+        AnCustomer.customerName = txtCustomerName.Text;
+        //store the address in the session object
+        Session["AnCustomer"] = AnCustomer;
+        //navigate to the view page
+        Response.Redirect("CustomerViewer.aspx");
     }
 }
