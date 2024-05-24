b595fee5340e0ca852542880c93f2de944fa0ac6
Adebayo Olowofoyeku <adebayoolowofoyeku@gmail.com>
2024-05-24 14:15:23 +0100

    Added the properties to the staff Class

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,9 +1,18 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
         public clsStaff()
         {
         }
+
+        public DateTime DateJoined { get; set; }
+        public int StaffID { get; set; }
+        public string StaffName { get; set; }
+        public string StaffRole { get; set; }
+        public int StaffSalary { get; set; }
+        public string StaffJobTitle { get; set; }
     }
 }
