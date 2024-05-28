605ee84673c9442efc73fbf6196133a55aba8e64
Adebayo Olowofoyeku <adebayoolowofoyeku@gmail.com>
2024-05-28 14:06:25 +0100

    Practical 8 done

--- AdminSystem/StaffViewer.aspx.cs
+++ AdminSystem/StaffViewer.aspx.cs
@@ -4,11 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //create a new instance of clsStaff
+        clsStaff AStaff = new clsStaff();
+        //get the data from the session object
+        AStaff = (clsStaff)Session["AStaff"];
+        //display the staff name for this entry
+        Response.Write(AStaff.StaffName);
+        Response.Write(AStaff.StaffRole);
+        Response.Write(AStaff.StaffSalary);
+        Response.Write(AStaff.StaffJobTitle);
+        Response.Write(AStaff.DateJoined);
     }
 }
