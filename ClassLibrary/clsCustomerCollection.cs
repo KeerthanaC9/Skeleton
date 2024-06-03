efe2fb282ffa5a642abab210ed4dd8b77b38ab4e
its-ym <161828938+its-ym@users.noreply.github.com>
2024-06-03 19:58:34 +0100

    UPDFYAE

--- ClassLibrary/clsCustomerCollection.cs
+++ ClassLibrary/clsCustomerCollection.cs
@@ -1,16 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
 namespace ClassLibrary
 {
     public class clsCustomerCollection
     {
+        // Declare the list and the current customer
+        List<clsCustomer> mCustomerList = new List<clsCustomer>();
+        clsCustomer mThisCustomer = new clsCustomer();
 
+        // Constructor
         public clsCustomerCollection()
         {
+          
 
+           
+        }
 
+        // Property to access the customer list
+        public List<clsCustomer> CustomerList
+        {
+            get { return mCustomerList; }
+            set { mCustomerList = value; }
+        }
 
+        // Property to get the count of customers
+        public int Count
+        {
+            get { return mCustomerList.Count; }
+            set { }
+        }
+
+        // Property to access the current customer
+        public clsCustomer ThisCustomer
+        {
+            get { return mThisCustomer; }
+            set { mThisCustomer = value; }
+        }
+
+        // Method to add a new customer (stub)
+        public int Add()
+        {
+            // Simulate adding a customer and returning a new ID
+            mThisCustomer.CustomerId = 123;
+            return mThisCustomer.CustomerId;
+        }
+
+        // Method to add test data for debugging purposes
+        private void AddTestData()
+        {
             clsCustomer TestItem = new clsCustomer();
             TestItem.CustomerId = 1;
             TestItem.CustomerName = "Andrew";
@@ -28,25 +67,6 @@ namespace ClassLibrary
             TestItem.CustomerPhoneNumber = "07865900876";
             TestItem.CustomerAddress = "56 Hempsy Lane";
             mCustomerList.Add(TestItem);
-
         }
-        List<clsCustomer> mCustomerList = new List<clsCustomer>();
-        public List<clsCustomer> CustomerList
-        {
-
-            get { return mCustomerList; }
-
-            set { mCustomerList = value; }
-        }
-
-        public int Count
-        {
-
-            get { return mCustomerList.Count; }
-
-            set { }
-        }
-        public clsCustomer ThisCustomer { get; set; }
-
     }
 }
